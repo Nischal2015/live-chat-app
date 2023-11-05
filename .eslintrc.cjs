87d2de341ef87beb0ba13f5750ab96daa943fc93
Nischal2015 <nischalshakya2015@gmail.com>
2023-11-05 14:47:59 +0545

    Add eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,23 +2,57 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json", "./tsconfig.vite.json"],
+    tsconfigRootDir: __dirname,
+  },
+  settings: {
+    react: { version: "detect" },
+    "import/resolver": {
+      typescript: {},
+    },
+  },
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
-    'no-restricted-imports': [
-      'error',
+    "no-restricted-imports": [
+      "error",
       {
-        patterns: ['@/features/*/*'],
+        patterns: ["@/features/*/*"],
       },
     ],
   },
+  overrides: [
+    {
+      files: ["./vite.config.ts"],
+      rules: {
+        "@typescript-eslint/prefer-nullish-coalescing": "off",
+      },
+    },
+    {
+      files: ["postcss.config.js", "tailwind.config.js"],
+      parserOptions: {
+        project: [],
+      },
+    },
+  ],
 };
